4906dfb60e4030e96d445c084ab135e39f6c57fe
Ivano Paulon <ivano_paulon@outlook.com>
2025-07-24 21:16:10 +0200

    Aggiunta migrazione Entity Framework _20250724_2115
    Sono state create due nuove classi di migrazione per il contesto di Entity Framework: `_20250724_2115` e il suo designer associato. La classe di migrazione include metodi vuoti `Up` e `Down` per future implementazioni. Il designer definisce il modello del database, specificando le entità e le loro proprietà, come `EntityChangeLog`, `BusinessParty`, `Address`, `Bank`, e altre, insieme alle relazioni tra di esse.

---
+++ Migrations/20250724191341_20250724_2115.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventForge.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20250724_2115 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
