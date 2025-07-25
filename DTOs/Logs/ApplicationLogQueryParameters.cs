ee8bc57d6e1a1e3d1529396801382e8300b1e8db
ivanopaulon <Ivano.Paulon@NBPAULON>
2025-07-25 11:17:29 +0200

    dto mancanti

---
+++ DTOs/Logs/ApplicationLogQueryParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace EventForge.DTOs.Logs
+{
+    public class ApplicationLogQueryParameters
+    {
+        public int? Page { get; set; }
+        public int? PageSize { get; set; }
+        public string? Level { get; set; }
+        public string? User { get; set; }
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+        // Altri parametri di ricerca se necessario
+    }
+}
