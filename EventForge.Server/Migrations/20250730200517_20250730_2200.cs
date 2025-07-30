8ed2faf8da2609ea9712dbb535eb01786ed84f12
Ivano Paulon <ivano_paulon@outlook.com>
2025-07-30 22:14:52 +0200

    Rimuovi proprietà 'Status' e aggiungi migrazione
    Modifiche al file `EventForgeDbContextModelSnapshot.cs` con rimozione delle proprietà `Status` da entità come `UMs` e `PaymentTerms`.
    Aggiunta della migrazione `_20250730_2200` per gestire la rimozione della colonna `Status` e creazione della classe parziale nel file `20250730200517_20250730_2200.Designer.cs` per aggiornare il modello di destinazione del contesto del database.

---
+++ EventForge.Server/Migrations/20250730200517_20250730_2200.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventForge.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20250730_2200 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "UMs");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "PaymentTerms");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "UMs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "PaymentTerms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
